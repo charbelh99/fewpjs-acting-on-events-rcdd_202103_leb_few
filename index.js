55fd0d5634d019fb5b3ec8512fc070433a353b11
Charbel Hbayter <charbel.hbayter@lau.edu>
2021-04-14 11:17:23 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Your code here
+document.addEventListener("keydown", function(e) {
+  if (e.key === "ArrowLeft") {
+    moveDodgerLeft();
+  }else if(e.key === "ArrowRight"){
+    moveDodgerRight();
+  }
+});
+function moveDodgerLeft() {
+  let leftNumbers = dodger.style.left.replace("px", "");
+  let left = parseInt(leftNumbers, 10);
+ 
+  if (left > 0) {
+    dodger.style.left = `${left - 1}px`;
+  }
+}
+function moveDodgerRight() {
+  let RightNumbers = dodger.style.right.replace("px", "");
+  let Right = parseInt(RightNumbers, 10);
+ 
+  if (Right > 0) {
+    dodger.style.right = `${Right - 1}px`;
+  }
+}
